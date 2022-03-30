52f8fbfd7b0e261c76a1660a16dfef302c681244
Harold <m.h.gamze@gmail.com>
2022-03-30 14:31:15 +0300

    next user updated

--- react-exercise/my-router/src/components/User.js
+++ react-exercise/my-router/src/components/User.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {useParams} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const User = () => {
 
     const[user, setUser] = useState({});
+    const[loading, setLoading] = useState(true)
     const {id} = useParams();
    
         
@@ -13,24 +14,23 @@ const User = () => {
 
     useEffect(()=> {
         axios(`https://jsonplaceholder.typicode.com/users/${id}`)
-        .then((res)=>setUser(res.data));
-        const myList = (JSON.stringify(user))
+        .then((res)=>setUser(res.data))
+        .finally(()=> setLoading(false));
+        // const myList = (JSON.stringify(user))
         // console.log(myList);
         // console.log(Object.keys(myList) + Object.values(myList));
 
 
-    },[]);
+    },[id]);
 
   return (
     <div>
         <h1>User Detail</h1>
-        
-        <code>
-            {JSON.stringify(user)}
-            
-        </code>
-    
-        
+        {loading && <div>Loading...</div>}
+        {!loading && <code>{JSON.stringify(user)}</code>}
+        <br/>
+        <br/>
+        <Link to={`/user/${parseInt(id) + 1}`}>Next User({parseInt(id) + 1})</Link>
 
     </div>
   )
