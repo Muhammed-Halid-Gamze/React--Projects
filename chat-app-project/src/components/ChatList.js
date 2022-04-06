481a5855d2ec558b99502c70d08552bd7ecafec8
Harold <m.h.gamze@gmail.com>
2022-04-06 11:05:51 +0300

    context  structure setted

---
+++ chat-app-project/src/components/ChatList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import ChatContext from '../context/ChatContext'
+// import { useContext } from 'react'
+
+const ChatList = () => {
+
+
+  return (
+    <div>ChatList</div>
+  )
+}
+
+export default ChatList
